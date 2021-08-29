4c5b1cd2834f3bc64f92538dedf9d7d023c4ef81
k4boom <mfzenbilci@gmail.com>
2021-08-29 12:38:24 +0300

    Auth System is added to the system.

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Product;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,11 +23,27 @@ use App\Http\Controllers\ProductsController;
 
 //But we dont need them
 
-Route::resource('products',ProductsController::class);
+//PUBLIC ROUTES
 
+Route::post('/login',[AuthController::class,'login']);
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/products','App\Http\Controllers\ProductsController@index');
+
+Route::get('/products/{id}','App\Http\Controllers\ProductsController@show');
 
 Route::get('/products/search/{name}',[ProductsController::class,'search']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::resource('products',ProductsController::class);
+
+
+//PROTECTED ROUTES
+
+Route::group(['middleware' => ['auth:sanctum'] ], function () {
+    Route::post('/products',[ProductsController::class,'store']);
+    Route::put('/products/{id}',[ProductsController::class,'update']);
+    Route::delete('/products/{id}',[ProductsController::class,'destroy']);
+    
+    Route::post('/logout',[AuthController::class,'logout']);
 });
